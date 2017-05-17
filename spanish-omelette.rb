1229599b1ccf4941df358ddc09bb51639d2ddbb6
jobvanduuren <job_van_duuren@hotmail.com>
2017-05-17 14:56:21 +0200

    print the ingredients

--- spanish-omelette.rb
+++ spanish-omelette.rb
@@ -15,6 +15,15 @@ ingredients = [
   { name: "potatoes", quantity: NUM_POTATOES},
   { name: "eggs", quantity: NUM_EGGS},
   { name: "onion", quantity: 1},
-  { name: "oil", quantity: "some"}
+  { name: "oil", quantity: "some"},
   { name: "salt", quantity: "some"}
 ]
+
+puts "Let's cook a Spanish Omelette"
+print_divider
+
+puts "You need the following ingredients:"
+
+ingredients.each do |ingredient|
+  puts "#{ingredient[:quantity]} #{ingredient[:name]}"
+end
